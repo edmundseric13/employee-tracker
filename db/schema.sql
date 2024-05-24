4939b58e69b03beb7279a07e75053f5c304f123a
Eric Edmunds <ecedmunds08@gmail.com>
2024-05-23 18:22:16 -0600

    some final touch ups

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,10 @@
-CREATE DATABASE employee_db;
-
-USE employee_db;
-
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
   department_id INT,
@@ -16,7 +12,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
--- db/seed.sql
+++ db/seed.sql
@@ -1,13 +1,11 @@
-USE employee_db;
+INSERT INTO department (name) VALUES
+('Sales'),
+('Engineering');
 
-INSERT INTO department (id, name) VALUES
-(1, 'Sales'),
-(2, 'Engineering');
+INSERT INTO role (title, salary, department_id) VALUES
+('Sales Lead', 100000, 1),
+('Software Engineer', 120000, 2);
 
-INSERT INTO role (id, title, salary, department_id) VALUES
-(1, 'Sales Lead', 100000, 1),
-(2, 'Software Engineer', 120000, 2);
-
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES
-(1, 'John', 'Doe', 1, NULL),
-(2, 'Jane', 'Doe', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+('John', 'Doe', 1, NULL),
+('Jane', 'Doe', 2, 1);
